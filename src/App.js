56716c6e36abf29c77dc75fb8e2ccbf711cdac0b
puzanovkirill <puzanovkirillofficial@gmail.com>
2022-02-16 18:38:30 +0500

    feat: dispatch main data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import useSWR, { SWRConfig } from 'swr';
+import useSWR from 'swr';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { setMainData } from './store/mainDataSlice';
 
-const fetcher = (...args) => fetch(...args).then((data) => data.json());
+export const fetcher = (...args) => fetch(...args).then((data) => data.json());
 
 function App() {
   const url =
-    'https://analytics.3divi.ru/api/v2/statistics/user/2088/devices/dates/ages/?key=d3aa35bde5ef46899b91aca9c66e174e&b=2020/03/08%2012:00:00&e=2020/09/08%2012:00:00&tzo=0';
-  const { data, error, isValidating } = useSWR(url, fetcher);
-  console.log(data, error);
+    'https://analytics.3divi.ru/api/v2/statistics/user/2088/devices/dates/ages/?key=d3aa35bde5ef46899b91aca9c66e174e' +
+    '&b=2020/03/08%2012:00:00&e=2020/09/08%2012:00:00&tzo=0';
+  const { data, error } = useSWR(url, fetcher);
+  const dispatch = useDispatch();
+  const mainData = useSelector((state) => state.mainData);
 
   if (error) {
     return <div>Error...</div>;
   }
-  if (isValidating) {
+
+  if (!mainData) {
     return <div>Loading...</div>;
   }
+  dispatch(setMainData(data));
 
-  return <pre className="App">{JSON.stringify(data, null, 2)}</pre>;
+  return <div className="App">{JSON.stringify(mainData)}</div>;
 }
 
 export default App;
