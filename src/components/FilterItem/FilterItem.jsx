beeb2d67c2e7dd985e854f52621e840c18444408
puzanovkirill <puzanovkirillofficial@gmail.com>
2022-02-17 17:51:46 +0500

    feat: filter item

---
+++ src/components/FilterItem/FilterItem.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import style from './FilterItem.module.css';
+import { useDispatch } from 'react-redux';
+import {
+  addFilteredItem,
+  removeFilteredItem,
+} from '../../store/filteredDataSlice';
+
+const FilterItem = ({ device }) => {
+  const [isActive, setIsActive] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isActive) {
+      dispatch(addFilteredItem(device));
+    }
+  }, []);
+
+  const handleClick = () => {
+    if (isActive) {
+      dispatch(removeFilteredItem(device));
+      setIsActive(!isActive);
+      return;
+    }
+    dispatch(addFilteredItem(device));
+
+    setIsActive(!isActive);
+  };
+
+  return (
+    <div className={style.filterItem} onClick={handleClick}>
+      <div
+        className={`${style.filterItem__name} ${isActive ? style.active : ''}}`}
+      >
+        {device.n}
+      </div>
+      <div className={isActive ? style.active : style.inactive}></div>
+    </div>
+  );
+};
+
+export default FilterItem;
